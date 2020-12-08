72c8850f71777c949a781e4ce56c9ffcc3d622f3
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2020-12-08 14:23:11 -0500

    Configure Redux

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Route exact path="/" component={props => {
+        return (
+          <div>
+            <Link to="/login">Login</Link>
+          </div>
+        );
+      }}/>
+      <Route exact path="/login" component={LoginForm}/>
     </div>
   );
 }
--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
