786839d0ccdf725a30a62cc301fb7c9be5b618d3
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2020-12-09 13:46:40 -0500

    Use HomePage as component for / route and use RegisterForm as component for /register route

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,16 @@
 import './App.css';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={props => {
-        return (
-          <div>
-            <Link to="/login">Login</Link>
-          </div>
-        );
-      }}/>
+      <Route exact path="/" component={HomePage}/>
       <Route exact path="/login" component={LoginForm}/>
+      <Route exact path="/register" component={RegisterForm}/>
     </div>
   );
 }
