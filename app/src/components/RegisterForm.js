992fd1fc930e7e779a801139c70a533b056076d7
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2020-12-11 13:23:47 -0500

    Refactor RegisterForm component with Reactstrap components

--- app/src/components/RegisterForm.js
+++ app/src/components/RegisterForm.js
@@ -2,33 +2,44 @@ import { useState } from 'react';
 import { connect } from 'react-redux';
 import { register } from '../actions';
 
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
 function RegisterForm(props) {
     let [username, setUsername] = useState('');
     let [password, setPassword] = useState('');
 
     return (
-        <form id="register-form" onSubmit={e => {
-            e.preventDefault();
-            props.register({ username, password });
-        }}>
-            <label>
-                Username
-                <br/>
-                <input type="text" onChange={e => {
-                    setUsername(e.target.value);
-                }}/>
-            </label>
-            <br/>
-            <label>
-                Password
-                <br/>
-                <input type="password" onChange={e => {
-                    setPassword(e.target.value);
-                }}/>
-            </label>
-            <br/>
-            <input type="submit"/>
-        </form>
+        <Form>
+            <FormGroup>
+                <Label for="username">Username</Label>
+                <Input
+                    type="text"
+                    name="username"
+                    id="username"
+                    onChange={e => {
+                        setUsername(e.target.value);
+                    }}
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="password">Password</Label>
+                <Input
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={e => {
+                        setPassword(e.target.value);
+                    }}
+                />
+            </FormGroup>
+            <Button
+                onClick={() => {
+                    props.register({ username, password });
+                }}
+            >
+                Submit
+            </Button>
+        </Form>
     );
 }
 
