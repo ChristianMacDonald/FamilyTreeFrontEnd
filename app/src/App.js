fa31d0eaef840725c4d1d415d8bb2c5ab2f1dddd
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2020-12-12 12:44:01 -0500

    Display Header component on all routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
+      <Route path="/" component={Header}/>
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/login" component={LoginPage}/>
     </div>
