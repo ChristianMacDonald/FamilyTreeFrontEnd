eab58740a38b96c1c502e2bea8985b9bc3185654
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2020-12-31 11:23:49 -0500

    Create authorizedAxios to generate axios objects with authorization header attached

---
+++ app/src/authorizedAxios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default function () {
+    const token = localStorage.getItem('token');
+    return axios.create({
+        headers: {
+            'authorization': token
+        }
+    });
+}
