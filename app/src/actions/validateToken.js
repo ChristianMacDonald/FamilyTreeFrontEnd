6ae99be6b7db9b5fe0a70ab94895e6959276d8c5
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2021-01-29 23:38:16 -0500

    Redirect to login page if token cannot be validated

--- app/src/actions/validateToken.js
+++ app/src/actions/validateToken.js
@@ -6,9 +6,10 @@ export const VALIDATE_TOKEN_ERROR = 'ERROR_TOKEN_ERROR';
 
 export const validateToken = () => dispatch => {
     dispatch({ type: VALIDATE_TOKEN_START, payload: true });
-    axios.post('https://cwm-family-tree.herokuapp.com/api/auth/token', { token: localStorage.getItem(token) }).then(res => {
+    axios.post('https://cwm-family-tree.herokuapp.com/api/auth/token', { token: localStorage.getItem('token') }).then(res => {
         dispatch({ type: VALIDATE_TOKEN_SUCCESS, payload: res });
     }).catch(err => {
         dispatch({ type: VALIDATE_TOKEN_ERROR, payload: err });
+        window.location.replace('http://localhost:3000/login');
     });
 }
