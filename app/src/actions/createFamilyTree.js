e7b6dd1de60a58a0f4ce66651312a01ea54d9998
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2021-02-03 03:07:50 -0500

    Create createFamilyTree action

--- app/src/actions/createFamilyTree.js
+++ app/src/actions/createFamilyTree.js
@@ -8,6 +8,7 @@ export const createFamilyTree = tree => dispatch => {
     dispatch({ type: CREATE_FAMILY_TREE_START, payload: true });
     authorizedAxios().post(`https://cwm-family-tree.herokuapp.com/api/users/${localStorage.getItem('username')}/family-trees`, tree).then(res => {
         dispatch({ type: CREATE_FAMILY_TREE_SUCCESS, payload: res });
+        window.location.reload();
     }).catch(err => {
         dispatch({ type: CREATE_FAMILY_TREE_ERROR, payload: err });
     });
