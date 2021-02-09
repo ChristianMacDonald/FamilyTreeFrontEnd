54f4cf0703d4dad782ccf9312abc0c70ce1fc6a6
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2021-02-08 23:30:56 -0500

    Obtain family tree ID for use as route parameter from familyMember argument in createFamilyMember action and delete familyTreeID parameter from definition of createFamilyMember

--- app/src/actions/createFamilyMember.js
+++ app/src/actions/createFamilyMember.js
@@ -4,9 +4,9 @@ export const CREATE_FAMILY_MEMBER_START = 'CREATE_FAMILY_MEMBER_START';
 export const CREATE_FAMILY_MEMBER_SUCCESS = 'CREATE_FAMILY_MEMBER_SUCCESS';
 export const CREATE_FAMILY_MEMBER_ERROR = 'CREATE_FAMILY_MEMBER_ERROR';
 
-export const createFamilyMember = (familyTreeID, familyMember) => dispatch => {
+export const createFamilyMember = familyMember => dispatch => {
     dispatch({ type: CREATE_FAMILY_MEMBER_START, payload: true });
-    authorizedAxios().post(`https://cwm-family-tree.herokuapp.com/api/users/${localStorage.getItem('username')}/family-trees/${familyTreeID}/family-members`, familyMember).then(res => {
+    authorizedAxios().post(`https://cwm-family-tree.herokuapp.com/api/users/${localStorage.getItem('username')}/family-trees/${familyMember.family_tree_id}/family-members`, familyMember).then(res => {
         dispatch({ type: CREATE_FAMILY_MEMBER_SUCCESS, payload: res });
     }).catch(err => {
         dispatch({ type: CREATE_FAMILY_MEMBER_ERROR, payload: err });
