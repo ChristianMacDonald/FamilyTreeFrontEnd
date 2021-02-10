681288565c01ae6c562e81a225b6c29edddece12
Christian MacDonald <chrismacd412@gmail.com>
2021-02-09 20:12:44 -0500

    Fix typo where type of action was set to familyTreeID rather than to DELETE_FAMILY_MEMBER_START

--- app/src/actions/deleteFamilyMember.js
+++ app/src/actions/deleteFamilyMember.js
@@ -6,7 +6,7 @@ export const DELETE_FAMILY_MEMBER_SUCCESS = 'DELETE_FAMILY_MEMBER_SUCCESS';
 export const DELETE_FAMILY_MEMBER_ERROR = 'DELETE_FAMILY_MEMBER_ERROR';
 
 export const deleteFamilyMember = (familyTreeID, familyMemberID) => dispatch => {
-    dispatch({ type: familyMemberID, payload: true });
+    dispatch({ type: DELETE_FAMILY_MEMBER_START, payload: true });
     authorizedAxios().delete(`https://cwm-family-tree.herokuapp.com/api/users/${localStorage.getItem('username')}/family-trees/${familyTreeID}/family-members/${familyMemberID}`).then(res => {
         dispatch({ type: DELETE_FAMILY_MEMBER_SUCCESS, payload: res });
     }).catch(err => {
